b8fbacde5ddc3c1c23933f0f92ac248c213b1a9a
andela-awofadeju <olayemi.awofadeju@andela.com>
2018-10-14 15:32:26 +0100

    build a simple todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='ui centered card'>
+      <div class='content'>
+        <div class='header'>
+          {{todo.title}}
+        </div>
+        <div class='meta'>
+          {{todo.task}}
+        </div>
+        <div class='extra content'>
+          <span class='right floated edit icon'>
+            <i class='edit icon'></i>
+          </span>
+        </div>
+      </div>
+      <div class='ui bottom attached green basic button' v-show="todo.done">
+        Completed
+      </div>
+      <div class='ui bottom attached red basic button'>Complete</div>
+    </div>
+</template>
+<script type="text/javascript">
+export default{
+  props: ['todo'],
+};
+</script>
